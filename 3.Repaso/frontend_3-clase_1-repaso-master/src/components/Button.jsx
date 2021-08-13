c80b019b18e6362a116094e5e4ebece56ee11be3
lmyanicelli <luciayanicelli@gmail.com>
2021-08-13 11:21:45 -0300

    3.Repaso Proyecto React refactorizado

---
+++ 3.Repaso/frontend_3-clase_1-repaso-master/src/components/Button.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import chuck from "../images/chuck.png"
+import "../App.css"
+
+/*const Button = ({defaultFrase,fraseUno,fraseDos,fraseTres,arregloDeFrases, onClick, fraseChuck}) => { 
+    const [textoBoton, setTextoBoton] = useState(defaultFrase)
+
+    const handleClick = () => {
+        setTextoBoton(onClick(arregloDeFrases,textoBoton,fraseUno,fraseDos,fraseTres))
+    }*/
+
+    //refactorizo
+    const Button = ({arregloDeFrases, onClick, fraseChuck}) => { 
+        const [textoBoton, setTextoBoton] = useState(arregloDeFrases[0])
+    
+        const handleClick = () => {
+            setTextoBoton(onClick(arregloDeFrases,textoBoton))
+        }    
+
+    return (
+        <div className="app__nuevaFrase">
+        <div>
+        <button  id="chuck_button" onClick={() => handleClick()} >{textoBoton}</button>
+        <p>{fraseChuck}</p>
+        </div>
+        <img id="chuck_norris" src={chuck} alt="chuck" />
+      </div>
+    )
+}
+
+export default Button
---
+++ 3.Repaso/frontend_3-clase_1-repaso-master/src/components/Listado.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import "../App.css"
+
+const Listado = ({ listadoDeFrases }) => {
+    const [frasesState, setFrasesState] = useState([])
+
+    useEffect(() => {
+        setFrasesState([...frasesState, listadoDeFrases])
+    }, [listadoDeFrases])
+
+    // EJERCICIO PARA REFACTORIZAR DESPUES DEL RETURN
+
+    //refactorizo con slice(-5) obtengo los últimos 5 elementos del array,
+    //reverse invierte el orden para q aparezca primero la última frase
+    //map crea el array con los elementos p
+    return (
+        <div className="app__viejasFrases">
+            <h3>ULTIMAS 5 FRASES</h3>
+            {frasesState.slice(-5).reverse().map(function (element, index) {
+        return <p>{element}</p>
+     })}
+            {/* {frasesState[0] ? <p>{frasesState[0]}</p> : null}
+        {frasesState[1] ? <p>{frasesState[1]}</p> : null}
+        {frasesState[2] ? <p>{frasesState[2]}</p> : null}
+        {frasesState[3] ? <p>{frasesState[3]}</p> : null}
+        {frasesState[4] ? <p>{frasesState[4]}</p> : null}
+        {frasesState[5] ? <p>{frasesState[5]}</p> : null} */}
+        </div>
+    )
+}
+
+export default Listado
